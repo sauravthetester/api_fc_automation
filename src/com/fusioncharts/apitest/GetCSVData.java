68dbd9a207966be440603253682095a2050d4f2c
saurav3046 <saurav@LPMB-Kol-Saurav.local>
2019-08-08 15:42:43 +0530

    merged 40+ APIs

---
+++ src/com/fusioncharts/apitest/GetCSVData.java
@@ -0,0 +1,91 @@
+package com.fusioncharts.apitest;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.fusioncharts.main.APITestBase;
+import com.fusioncharts.pom.APIPageObjectModel;
+import com.fusioncharts.util.TestUtil;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class GetCSVData extends APITestBase {
+	
+	//The api name according to the data sheet
+	private final static String apiName = "getCSVData()"; 
+	Object[][] data;
+	APIPageObjectModel pom;
+	
+	@BeforeTest
+	public void setUp() 
+	{
+		api = new APITestBase();
+		api.initialize();
+		pom = new APIPageObjectModel();
+		data = TestUtil.getTestData();
+	}
+	  
+	@Test(priority = 1)
+	public void verifyAPIExistsInDataSheetGetCSVData()
+	{
+		boolean apiExists = TestUtil.thisAPIexists(data, apiName);
+		Assert.assertTrue(apiExists, "API name matches in data sheet");
+	}
+	
+	@Test(priority = 2)
+	public void verifyChartIsRenderedGetCSVData()
+	{
+		String htmlData = TestUtil.chartHtml(data, apiName);
+		TestUtil.htmlWrite(htmlData);
+		driver.navigate().refresh();
+		boolean containerDisplayed = pom.verifyIfChartMainContainerDisplayed();
+		Assert.assertTrue(containerDisplayed, "chart is rendered");
+	}
+	
+	@Test(priority = 3)
+	public void verifyAPIGetCSVData() throws IOException
+	{
+		String apiScript = TestUtil.apiScript(data, apiName);
+		
+		driver.findElement(By.id("get_chart_data")).click();
+		String msgText = driver.findElement(By.id("msg")).getText();
+		
+		Assert.assertTrue(!msgText.isEmpty(), "CSV data should have some value");
+		
+		String[] lines = msgText.split("\\r?\\n");
+        for (String line : lines) 
+        {
+            Assert.assertTrue(line.startsWith("\""), "CSV is in correct format");
+            Assert.assertTrue(line.endsWith("\""), "CSV is in correct format");
+            Assert.assertTrue(line.contains("\",\""), "CSV is in correct format");
+        }
+        
+        test.log(LogStatus.PASS, test.addScreenCapture(APITestBase.capture("GetCSVData_CSV data should show above chart")));	//Code Line for screenshot
+	}
+	
+	@AfterTest
+	public void shutDown() 
+	{
+		try
+		{
+			System.out.println("getCSVData() executed");
+			Thread.sleep(3000);
+		}
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+		report.endTest(test);
+		report.flush();
+		driver.quit();
+	}
+
+
+}
